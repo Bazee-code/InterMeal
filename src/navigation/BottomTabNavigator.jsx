d1dd47889f0ca8555a1129e20a928d0c5cc48c29
Bazee-code <eugeneobazee2@gmail.com>
2024-04-22 19:22:43 +0300

    Update bottom tab icons

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,9 +1,9 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import {createMaterialBottomTabNavigator} from 'react-native-paper/react-navigation';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as Routes from './routes';
 import Icon from 'react-native-vector-icons/AntDesign';
+import Icon1 from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   DashboardScreenStack,
   InsightsScreenStack,
@@ -36,32 +36,33 @@ const BottomTabNavigator = () => {
         }}
       />
       <BottomTab.Screen
-        name={Routes.INSIGHTS_TAB}
-        component={InsightsScreenStack}
-        options={{
-          tabBarLabel: 'Timer',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="calendar" color={color} size={size} />
-          ),
-        }}
-      />
-      <BottomTab.Screen
         name={Routes.TIMER_TAB}
         component={TimerScreenStack}
         options={{
           tabBarLabel: 'Timer',
           tabBarIcon: ({color, size}) => (
-            <Icon name="calendar" color={color} size={size} />
+            <Icon1 name="timer-outline" color={color} size={size} />
           ),
         }}
       />
+      <BottomTab.Screen
+        name={Routes.INSIGHTS_TAB}
+        component={InsightsScreenStack}
+        options={{
+          tabBarLabel: 'Insights',
+          tabBarIcon: ({color, size}) => (
+            <Icon2 name="lightbulb-on-outline" color={color} size={size} />
+          ),
+        }}
+      />
+
       <BottomTab.Screen
         name={Routes.PROFILE_TAB}
         component={ProfileScreenStack}
         options={{
           tabBarLabel: 'Profile',
           tabBarIcon: ({color, size}) => (
-            <Icon name="calendar" color={color} size={size} />
+            <Icon name="user" color={color} size={size} />
           ),
         }}
       />
